7bfca2f23e18234c39db62da04596a79ae0151b8
Marcelo <masoft2016@gmail.com>
2021-09-27 18:52:56 -0300

    muchas mas tablas

---
+++ src/main/java/ar/com/masoft/assisrest/Obrasocial.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.masoft.assisrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.TableGenerator;
+import javax.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ *
+ * @author magug
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Obrasocial implements java.io.Serializable {
+    @Id
+    @TableGenerator(name = "oss",
+            table = "numerador",
+            pkColumnName = "tipo",
+            valueColumnName = "numero",
+            pkColumnValue = "OSS",
+            allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "oss")
+    @Basic(optional = false)
+    @Column(name = "OSS_ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "OSS_NOMBRE", length=80)
+    private String nombre;
+    
+    @CreatedBy
+    @Column(name = "AUD_USUALTA", length=50)
+    private String user;
+    @LastModifiedBy 
+    @Column(name = "AUD_USUMOD" , length=50)
+    private String userMod;
+    @CreatedDate 
+    @Column(name = "AUD_FECHALTA")    
+    private Long audFechaAlta;
+    @LastModifiedDate
+    @Column(name = "AUD_FECHAMOD")    
+    private Long audFechaMod;
+    private @Version @JsonIgnore Long version;
+}
